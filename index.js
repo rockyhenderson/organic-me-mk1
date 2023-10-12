fffbaa58bcd2d6adb94d6658401ead95b2f8b325
rockyhenderson <jack@teamhendo.com>
2023-10-12 21:51:19 +0100

    added honey slider

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-function mainContent(){
-    
+$(document).ready(function() {
+    $("#honey-onclick-content").hide();
+});
+
+function mainContent() {
+    $("#honey-onclick-content").slideDown();
+}
+function hideMainContent(){
+    $("#honey-onclick-content").hide();
 }
