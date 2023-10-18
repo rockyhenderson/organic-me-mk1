d0ea2bfd11249a017eea808161f086d50a0bd255
rockyhenderson <jack@teamhendo.com>
2023-10-18 23:02:01 +0100

    display divs on button

--- about-us-script.js
+++ about-us-script.js
@@ -11,13 +11,22 @@ const StaffContent = document.getElementById("staff-section");
 
 
 ValueButton.addEventListener("click", () => {
+    document.getElementById('value-section').style.display = "block"
+    document.getElementById('staff-section').style.display = "none"
+    document.getElementById('history-section').style.display = "none"
     ValueContent.scrollIntoView({ behavior: "smooth" });
 });
 
 StaffButton.addEventListener("click", () => {
+    document.getElementById('value-section').style.display = "none"
+    document.getElementById('staff-section').style.display = "block"
+    document.getElementById('history-section').style.display = "none"
     StaffContent.scrollIntoView({ behavior: "smooth" });
 });
 
 HistoryButton.addEventListener("click", () => {
+    document.getElementById('value-section').style.display = "none"
+    document.getElementById('staff-section').style.display = "none"
+    document.getElementById('history-section').style.display = "block"
     HistoryContent.scrollIntoView({ behavior: "smooth" });
 });
