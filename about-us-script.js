05c84716c3e686048ec371d63db854e71e21179f
Jack Henderson <jack@teamhendo.com>
2023-10-23 08:58:10 +0100

    FINNALY FIXED TEXT

--- about-us-script.js
+++ about-us-script.js
@@ -2,23 +2,27 @@ const cardItems = document.querySelectorAll(".card-item");
 const pieItems = document.querySelectorAll(".pie");
 let currentIndex = 0;
 
-
 cardItems[currentIndex].style.display = "block";
 pieItems[currentIndex].classList.add("active-pie");
 
 function displayNextCard() {
-
   cardItems[currentIndex].style.display = "none";
   pieItems[currentIndex].classList.remove("active-pie");
 
-
   currentIndex = (currentIndex + 1) % cardItems.length;
-
 
   cardItems[currentIndex].style.display = "block";
   pieItems[currentIndex].classList.add("active-pie");
 }
 
+function displayNextCardOnClick() {
+  displayNextCard();
+}
+
 cardItems.forEach((card, index) => {
-  card.addEventListener("click", displayNextCard);
+  card.addEventListener("click", displayNextCardOnClick);
+});
+
+pieItems.forEach((pie, index) => {
+  pie.addEventListener("click", displayNextCardOnClick);
 });
