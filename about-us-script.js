f5ee21437011a9194c2dba000da7c1fc984f5e43
Jack Henderson <jack@teamhendo.com>
2023-10-23 10:50:46 +0100

    History section func but not good looking

--- about-us-script.js
+++ about-us-script.js
@@ -1,28 +1,34 @@
 const cardItems = document.querySelectorAll(".card-item");
 const pieItems = document.querySelectorAll(".pie");
+const histLine = document.querySelectorAll("#history-line")
 let currentIndex = 0;
 
 cardItems[currentIndex].style.display = "block";
 pieItems[currentIndex].classList.add("active-pie");
 
-function displayNextCard() {
+function displayNextCard(index) {
   cardItems[currentIndex].style.display = "none";
   pieItems[currentIndex].classList.remove("active-pie");
-
-  currentIndex = (currentIndex + 1) % cardItems.length;
-
+  //do not put anything in this gap for some reason when i do all of my content disapers
+  currentIndex = index % cardItems.length;
   cardItems[currentIndex].style.display = "block";
   pieItems[currentIndex].classList.add("active-pie");
+
+ 
+  for (let i = 0; i <= index; i++) {
+    pieItems[i].classList.add("active-pie");
+  }
 }
 
-function displayNextCardOnClick() {
-  displayNextCard();
+function displayNextCardOnClick(index) {
+  displayNextCard(index);
+  console.log(`Clicked pie number ${index}`);
 }
 
 cardItems.forEach((card, index) => {
-  card.addEventListener("click", displayNextCardOnClick);
+  card.addEventListener("click", () => displayNextCardOnClick(index));
 });
 
 pieItems.forEach((pie, index) => {
-  pie.addEventListener("click", displayNextCardOnClick);
+  pie.addEventListener("click", () => displayNextCardOnClick(index));
 });
