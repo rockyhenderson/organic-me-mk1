9fe37bfbc9a6b8f6c3db7749e0e2874586a7d950
rockyhenderson <jack@teamhendo.com>
2023-10-27 20:17:04 +0100

    Fixed Nav

--- about-us-script.js
+++ about-us-script.js
@@ -346,9 +346,12 @@ function toggleNavbarNavAltMarkup() {
   var navbarNavAltMarkup = document.getElementById("navbarNavAltMarkup");
 
   if (navbarNavAltMarkup.classList.contains("show")) {
+    navbar.style.backgroundColor = 'transparent';
     navbarNavAltMarkup.classList.remove("show");
+    
   } else {
     navbarNavAltMarkup.classList.add("show");
+    navbar.style.backgroundColor = 'white';
   }
 }
 
