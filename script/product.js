e7b69db1e89ade8abe01c53541a6fcbe5227c7bb
rockyhenderson <jack@teamhendo.com>
2023-10-31 20:07:34 +0000

    added content sorting system

---
+++ script/product.js
@@ -0,0 +1,113 @@
+function showMeat(){
+    alert("MEAT")
+}
+function showHoney(){
+    alert("HONEY")
+}
+function showVeg(){
+    alert("VEG")
+}
+
+$(document).ready(function(){
+    $.ajax({
+      url: '../product-section-content/products.json',
+      type: 'GET',
+      dataType: 'json',
+      success: function (data){
+        products = data;
+        //thank fuck this works
+        
+      },
+      error: function(error){
+        console.log('Error loading the JSON File')
+      }
+    })
+  
+  
+  
+  });
+    
+  
+  function SortByPrice() { //initilize function
+      products.sort((a, b) => a.price - b.price); //sorts by price :)
+      createDiv()
+  }
+  function SortByName(){ //starts function
+      products.sort((a, b) => a.name.localeCompare(b.name)); // sorts by name
+      createDiv()
+  }
+  // Attach the searchProducts function to the input's "keyup" event using jQuery
+  
+  function createDiv(){
+      $("#productList").html(""); //clears div
+  
+      for (var i = 0; i < products.length; i++) {
+          var product = products[i];
+  
+          // makes a div for each item
+          var div = $('<div class="product-box">');
+  
+          // loads dog!
+          var img = $('<img class="meat-img">').attr({
+              src: '/img/' + product.image, // loads specific img
+              alt: 'photo of ' + product.name, // name in alt text for accesibility'
+            });
+          // adds all the info
+  
+          var h2 = $('<h2>').text(product.name)
+          var h3 = $('<h3>').text("£" + product.price + "/per pound")
+          // adds both to div
+          div.append(img, h2, h3);
+  
+          // adds div to container
+          $('#productList').append(div);
+      }
+  }
+  function updateProductList() {
+    $("#productList").empty(); // Clear existing divs
+  
+    const searchText = $('#search-input').val().toLowerCase();
+    
+    const filteredProducts = products.filter((product) =>
+      product.name.toLowerCase().startsWith(searchText)
+    );
+    console.log(filteredProducts)
+    if (filteredProducts.length === 0) {
+      var div = $('<div class="product-box">');
+      var h2 =  $('<h2>').text("We dont have anything like this yet! Check back as we update regularly :)")
+      div.append(h2)
+      $('#productList').append(div);
+    }
+    else{
+      for (var i = 0; i < filteredProducts.length; i++) {
+        var product = filteredProducts[i];
+    
+        // Create a div for each item
+        var div = $('<div class="product-box">');
+    
+        // Load the product image
+        var img = $('<img class="meat-img">').attr({
+          src: 'img/' + product.image, // Assuming the images are in a folder named 'img'
+          alt: 'photo of ' + product.name, // Name in alt text for accessibility
+        });
+    
+        // Create a paragraph element to display product information
+        var h2 = $('<h2>').text(product.name)
+        var h3 = $('<h3>').text("£" + product.price + "/per pound")
+        // adds both to div
+        div.append(img, h2, h3);
+    
+        // Add the div to the container with the ID 'productList'
+        $('#productList').append(div);
+      }
+    }
+    
+  }
+  $(document).ready(function () {
+    $("#search-input").on("input", function () {
+      updateProductList();
+    });
+  });
+  
+  
+  
